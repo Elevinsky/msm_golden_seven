b00aff0aa040a4aadb90b5b62cd562f650426008
Erica Levinsky <elevinsky@gmail.com>
2016-11-01 20:44:52 -0500

    added director index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  
+
+  #director routes
+  get("/directors", :controller => 'directors', :action => "index")
+  get("/directors/:id", :controller => 'directors', :action => "show")
+
+  get("/directors/delete_director/:id", :controller => 'directors', :action => "destroy")
+
+  get("/directors/new", :controller => 'directors', :action => "new_form")
+  get("/directors/new", :controller => 'directors', :action => "create_row")
+
+
 end
