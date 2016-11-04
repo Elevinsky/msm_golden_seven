898cb39d599c774d4ce80861431df9c44064a0fa
Erica Levinsky <elevinsky@gmail.com>
2016-11-04 18:49:31 -0500

    director list view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get("/", :controller => 'directors', :action => "index")
 
   #director routes
   get("/directors", :controller => 'directors', :action => "index")
