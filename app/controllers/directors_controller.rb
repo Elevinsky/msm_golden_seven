eba746e434aece6bbeb09b4f8aa8e5950c4bf923
Erica Levinsky <elevinsky@gmail.com>
2016-11-04 19:52:35 -0500

    added edit

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -7,4 +7,20 @@ class DirectorsController < ApplicationController
     @director = Director.find_by({ :id => params[:id] })
   end
 
+  def edit_form
+    @director = Director.find_by({ :id => params[:id] })
+  end
+
+  def update_row
+    @director = Director.find_by({ :id => params[:id] })
+
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
+
+    @director.save
+
+    render("show")
+  end
 end
