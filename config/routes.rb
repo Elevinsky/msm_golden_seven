8550a09c7684b572fc8c99a6f11ebaa82be31c24
Erica Levinsky <elevinsky@gmail.com>
2016-11-04 20:49:33 -0500

    added actors functionality

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,27 @@ Rails.application.routes.draw do
 
   get("/directors/new_form", :controller => 'directors', :action => "new_form")
   get("/create_director", :controller => 'directors', :action => "create_row")
-  
+
   get("/directors", :controller => 'directors', :action => "index")
   get("/directors/:id", :controller => 'directors', :action => "show")
 
   get("/directors/:id/edit_form", :controller => 'directors', :action => "edit_form")
   get("/update_director/:id", :controller => 'directors', :action => "update_row")
+
   get("/directors/delete_director/:id", :controller => 'directors', :action => "destroy")
 
+  #actor routes
 
+  get("/actors/new_form", :controller => 'actors', :action => "new_form")
+  get("/create_actor", :controller => 'actors', :action => "create_row")
+
+  get("/actors", :controller => 'actors', :action => "index")
+  get("/actors/:id", :controller => 'actors', :action => "show")
+
+  get("/actors/:id/edit_form", :controller => 'actors', :action => "edit_form")
+  get("/update_actor/:id", :controller => 'actors', :action => "update_row")
+
+  get("/actors/delete_actor/:id", :controller => 'actors', :action => "destroy")
 
 
 end
